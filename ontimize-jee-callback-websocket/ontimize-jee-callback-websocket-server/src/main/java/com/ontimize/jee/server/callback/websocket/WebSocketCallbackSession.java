4a5aee48574535b7828449bdf61b9da45f28a8f7
faustino.lage <faustino.lage@imatia.com>
2018-03-15 17:25:38 +0000

    Version 1.0.1
    git-svn-id: https://svn.imatia.com/ontimize/ontimize-jee-callback-websocket/tags/1.0.1@3735 c65a67e3-ea31-493c-9319-7ebef4ec2fa5

---
+++ ontimize-jee-callback-websocket/ontimize-jee-callback-websocket-server/src/main/java/com/ontimize/jee/server/callback/websocket/WebSocketCallbackSession.java
@@ -0,0 +1,34 @@
+package com.ontimize.jee.server.callback.websocket;
+
+import org.springframework.web.socket.WebSocketSession;
+
+import com.ontimize.jee.server.callback.CallbackSession;
+
+/**
+ * The Class WebSocketCallbackSession.
+ */
+public class WebSocketCallbackSession implements CallbackSession {
+
+	/** The native session. */
+	private final Object nativeSession;
+
+	/**
+	 * Instantiates a new web socket callback session.
+	 *
+	 * @param session
+	 *            the session
+	 */
+	public WebSocketCallbackSession(WebSocketSession session) {
+		this.nativeSession = session;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.ontimize.jee.server.callback.CallbackSession#getNativeSession()
+	 */
+	@Override
+	public Object getNativeSession() {
+		return this.nativeSession;
+	}
+
+}
